415ec484500706a9778e44b455d8ccac7239364d
mxnu2001 <manupradeepkumar4iphone@gmail.com>
2025-09-14 13:11:32 +0530

    google sheets2

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import pandas as pd
 import gspread
 from gspread_dataframe import set_with_dataframe, get_as_dataframe
 from oauth2client.service_account import ServiceAccountCredentials
+import json
+
 
 # --- Google Sheets setup ---
 SHEET_NAME = "Dermatology_QA"
 JSON_KEY_FILE = "service_account.json"  # Upload this JSON to your repo or Streamlit secrets
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(JSON_KEY_FILE, scope)
+
+# Load credentials from Streamlit secrets
+creds_dict = json.loads(st.secrets["google_creds"]["json"])
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 sheet = client.open(SHEET_NAME).sheet1
